bd67d33f69c6f74c4d08478b770379ffd3dbe528
Rahmat <rahmatdw48@gmail.com>
2021-12-20 19:19:05 +0700

    no message

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <hr>
     <div class="container">
         <div class="row">
-            <div class="col text-center" style="padding-bottom: 10px;">
+            <div class="col text-center">
                 Copyright @2021 Team Durian Musang King
             </div>
         </div>
