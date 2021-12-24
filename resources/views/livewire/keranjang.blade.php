2e445105d819bee7e244c6fb87db923b4393af86
DESKTOP-JINUVIU\Bas-PC <mahesa.23@students.amikom.ac.id>
2021-12-24 14:43:50 +0700

    no message

--- resources/views/livewire/keranjang.blade.php
+++ resources/views/livewire/keranjang.blade.php
@@ -12,7 +12,78 @@
                 </ol>
             </nav>
         </div>
-    
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message')}}
+            </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td>No</td>
+                            <td>Gambar</td>
+                            <td>Nama</td>
+                            <td>Limited Edition</td>
+                            <td>Warna</td>
+                            <td>Model</td>
+                            <td><strong>Total Harga</strong></td>
+                            <td>Hapus</td>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $no = 1 ?>
+                        @forelse($pesanan_detail as $pesanan_details)
+                        <tr>
+                            <td>{{$no++}}</td>
+                            <td>
+                                <img src="{{ url('asset/product')}}/{{$pesanan_details->product->gambar}}" alt="" class="img-fluid" width="200">
+                            </td>
+                            <td>
+                                {{$pesanan_details->product->nama}}
+                            </td>
+                            <td>
+                               <?php 
+                                if(!$pesanan_details->limitededition)
+                                {
+                                    echo "NO";
+                                }
+                                else {
+                                    echo "YES";
+                                }
+                               ?>
+                            </td>
+                            <td>
+                                {{$pesanan_details->warna}}
+                            </td>
+                            <td>
+                                {{$pesanan_details->model}}
+                            </td>
+                            <td><strong>
+                                Rp.  {{number_format($pesanan_details->total_harga)}}
+                            </strong>
+                             
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="7">Data Kosong</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 
