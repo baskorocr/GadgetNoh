68010b1cc0652f3aef81a83c88c5b2d6d7b59907
DESKTOP-JINUVIU\Bas-PC <mahesa.23@students.amikom.ac.id>
2022-01-08 01:30:28 +0700

    no message

--- app/Http/Livewire/AdminHome.php
+++ app/Http/Livewire/AdminHome.php
@@ -4,12 +4,16 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use File;
 
 class AdminHome extends Component
 {
     public function destroy($Id){
+        
         $p = Product::find($Id);
+        File::delete('asset/product/'.$p->gambar);
         $p->delete();
+        
         session()->flash('message', 'Barang berhasil dihapus');
         return redirect()->back();
     }
